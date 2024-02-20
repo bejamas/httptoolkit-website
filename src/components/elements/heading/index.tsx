dc467972e7abe42bc81ca1e3fe6665b91cfee837
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-02-20 15:10:40 -0400

    feat(HTD-5): add line-height variants and text align

--- src/components/elements/heading/index.tsx
+++ src/components/elements/heading/index.tsx
@@ -4,10 +4,18 @@ import { StyledHeading } from './heading.styles';
 import type { HeadingProps } from './heading.types';
 
 export const Heading = forwardRef<HTMLDivElement, Component<HeadingProps>>((props, ref) => {
-  const { children, className, as = 'h1', fontSize = 'xl', fontWeight, color } = props;
+  const { children, className, as = 'h1', fontSize = 'xl', fontWeight, color, textAlign } = props;
 
   return (
-    <StyledHeading {...ref} as={as} fontSize={fontSize} color={color} fontWeight={fontWeight} className={className}>
+    <StyledHeading
+      {...ref}
+      as={as}
+      fontSize={fontSize}
+      color={color}
+      fontWeight={fontWeight}
+      className={className}
+      textAlign={textAlign}
+    >
       {children}
     </StyledHeading>
   );
--- src/components/elements/text/index.tsx
+++ src/components/elements/text/index.tsx
@@ -4,10 +4,18 @@ import { StyledText } from './text.styles';
 import type { TextProps } from './text.types';
 
 export const Text = forwardRef<HTMLDivElement, Component<TextProps>>((props, ref) => {
-  const { children, className, as = 'p', fontSize = 'xl', fontWeight, color } = props;
+  const { children, className, as = 'p', fontSize = 'xl', fontWeight, color, textAlign } = props;
 
   return (
-    <StyledText {...ref} as={as} fontSize={fontSize} color={color} fontWeight={fontWeight} className={className}>
+    <StyledText
+      {...ref}
+      as={as}
+      fontSize={fontSize}
+      color={color}
+      fontWeight={fontWeight}
+      className={className}
+      textAlign={textAlign}
+    >
       {children}
     </StyledText>
   );
