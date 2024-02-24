4fbe30b2bcc12d22167a75440b34a9b7872e0062
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-02-24 12:08:14 -0400

    fix(HTD-10): remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { Badge } from '@/components/elements/badge';
 import { Button } from '@/components/elements/button';
 import { Container } from '@/components/elements/container';
 import { Heading } from '@/components/elements/heading';
-import { Sparkle, Alien, Logo, CaretDown, RocketLaunch } from '@/components/elements/icon/index';
+import { Sparkle, Alien, Logo, CaretDown, RocketLaunch } from '@/components/elements/icon';
 import ShowCase from '@/components/elements/showcase';
 import { SquareIcon } from '@/components/elements/square-icon';
 import { Text } from '@/components/elements/text';
