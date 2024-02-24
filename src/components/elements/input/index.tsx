08b8a22bd5996236f980ae8fd9c1edce43303198
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-02-24 09:37:20 -0400

    feat(HTD-11): add use ref

--- src/components/elements/input/index.tsx
+++ src/components/elements/input/index.tsx
@@ -1,18 +1,24 @@
+import { forwardRef } from 'react';
+
 import { StyledInput, StyledInputBorder, StyledInputWrapper } from './input.styles';
 import type { InputProps } from './input.types';
 import { Text } from '../text';
 
-export const Input = ({ as = 'input', errorMessage, hasError = false, ...props }: InputProps) => {
-  return (
-    <StyledInputWrapper>
-      <StyledInputBorder hasError={hasError} styledAs={as}>
-        <StyledInput hasError={hasError} as={as} {...props} />
-      </StyledInputBorder>
-      {errorMessage && (
-        <Text fontSize="s" color="cinnarbarRed">
-          {errorMessage}
-        </Text>
-      )}
-    </StyledInputWrapper>
-  );
-};
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ as = 'input', errorMessage, hasError = false, ...props }, ref) => {
+    return (
+      <StyledInputWrapper>
+        <StyledInputBorder hasError={hasError} styledAs={as}>
+          <StyledInput ref={ref} hasError={hasError} as={as} {...props} />
+        </StyledInputBorder>
+        {errorMessage && (
+          <Text fontSize="s" color="cinnarbarRed">
+            {errorMessage}
+          </Text>
+        )}
+      </StyledInputWrapper>
+    );
+  },
+);
+
+Input.displayName = 'Input';
