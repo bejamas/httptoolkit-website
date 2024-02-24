1cda076502b31d867e8b45393028f5e20849fdde
Gerald Martinez <gerald@bejamas.io>
2024-02-23 22:33:20 -0600

    feat(HTD-13): remove unused ts nocheck

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-//@ts-nocheck
-
 import styled, { createGlobalStyle, css, ThemeProvider } from 'styled-components';
 import reset from 'styled-reset';
 
