2ad83062873eb29f53d2563c3d922a565b1119cf
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-02-27 20:01:21 -0400

    feat(HTD-19): add copy element

--- src/components/elements/icon/index.ts
+++ src/components/elements/icon/index.ts
@@ -1,5 +1,14 @@
-import { Alien, CaretDown, RocketLaunch, Sparkle, Book, MagnifyingGlass } from '@phosphor-icons/react/dist/ssr';
+import {
+  Alien,
+  CaretDown,
+  RocketLaunch,
+  Sparkle,
+  Book,
+  MagnifyingGlass,
+  Copy,
+  CheckCircle,
+} from '@phosphor-icons/react/dist/ssr';
 
 import { FullLogo, Logo } from './custom';
 
-export { Alien, CaretDown, RocketLaunch, Sparkle, Book, FullLogo, Logo, MagnifyingGlass };
+export { Alien, CaretDown, RocketLaunch, Sparkle, Book, FullLogo, Logo, MagnifyingGlass, Copy, CheckCircle };
