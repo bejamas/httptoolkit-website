1b01b22caefb718195eb16844c00094a1a32d5ab
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-06 11:49:26 -0400

    fix(HTD-35): add pagination for github download stats

--- src/lib/services/get-github-download-stats.ts
+++ src/lib/services/get-github-download-stats.ts
@@ -1,21 +1,33 @@
 import sum from 'lodash/sum';
 
-export async function getGithubDownloadStats() {
-  const response = await fetch('https://api.github.com/repos/httptoolkit/httptoolkit-desktop/releases?per_page=100', {
-    headers: {
-      Accept: 'application/vnd.github.v3+json',
-    },
-  });
+import { GITHUB_ORG } from '../constants/github';
 
-  if (!response.ok) {
-    response.text().then(console.log); // Async print the body when we have it
-    throw new Error(`Bad response for GitHub stats: ${response.status}`); // Kill the build
+export async function getGithubDownloadStats() {
+  const releases = [];
+  let pageIndex = 1;
+  while (true) {
+    const response = await fetch(
+      `https://api.github.com/repos/${GITHUB_ORG}/httptoolkit-desktop/releases?per_page=100&page=${pageIndex}`,
+      {
+        headers: {
+          Accept: 'application/vnd.github.v3+json',
+        },
+      },
+    );
+    if (!response.ok) {
+      response.text().then(console.log); // Async print the body when we have it
+      throw new Error(`Bad response for GitHub stats: ${response.status}`); // Kill the build
+    }
+
+    const releasesPage = await response.json();
+    releases.push(...releasesPage);
+
+    if (releasesPage.length < 100) break;
+    else pageIndex += 1;
   }
 
-  const releaseStats = await response.json();
-
   const totalDownloads = sum(
-    releaseStats.map((release: any) => sum(release.assets.map((asset: any) => asset.download_count))),
+    releases.map((release: any) => sum(release.assets.map((asset: any) => asset.download_count))),
   );
 
   return totalDownloads;
