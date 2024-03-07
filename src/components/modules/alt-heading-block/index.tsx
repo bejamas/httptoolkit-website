061ddf6085b9eb1935eb6d026a12cb1a7f2b8a60
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-07 10:05:07 -0400

    feat(HTD-42): reuse highlight logic

--- src/components/modules/alt-heading-block/index.tsx
+++ src/components/modules/alt-heading-block/index.tsx
@@ -6,25 +6,19 @@ import {
 import type { AltHeadingBlockProps } from './alt-heading-block.types';
 
 import type { HeadingProps } from '@/components/elements/heading/heading.types';
+import { HighlightedText } from '@/components/elements/highlighted-text';
 
 /**
  * @param title: This string can have highlighted text on the segments that are between \`\*content\*\`
  */
 export const AltHeadingBlock = ({ title, subtitle, mediumHeading = false }: AltHeadingBlockProps) => {
   const headingSize: HeadingProps['fontSize'] = mediumHeading ? 'm' : 'l';
-  const formattedTitle = title.split('`').map(segment => {
-    const isHighlighted = segment.includes('*');
-    if (!isHighlighted) {
-      return segment;
-    }
-    return <span>{segment.replaceAll('*', '')}</span>;
-  });
 
   return (
     <StyledAltHeadingBlockWrapper>
       {subtitle && <StyledAltHeadingBlockSubtitle>{subtitle}</StyledAltHeadingBlockSubtitle>}
       <StyledAltHeadingBlockTitle forwardedAs="h2" fontSize={headingSize} color="textGradient" textAlign="center">
-        {formattedTitle}
+        <HighlightedText title={title} />
       </StyledAltHeadingBlockTitle>
     </StyledAltHeadingBlockWrapper>
   );
