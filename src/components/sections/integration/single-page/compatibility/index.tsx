de7c4f78ebd658381f3cdbe1e51bd951098a0815
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-08 08:20:49 -0400

    feat(HTD-44): add compatibility component

---
+++ src/components/sections/integration/single-page/compatibility/index.tsx
@@ -0,0 +1,41 @@
+import { CompatibilityBadge } from './badge';
+import {
+  StyledCompatibilityBadgeWrapper,
+  StyledIntegrationCompatibilityContent,
+  StyledIntegrationCompatibilityContentTitle,
+  StyledIntegrationCompatibilityContentWrapper,
+  StyledIntegrationCompatibilityGradientWrapper,
+  StyledIntegrationCompatibilityLabel,
+  StyledIntegrationCompatibilityWrapper,
+} from './compatibility.styles';
+import type { IntegrationCompatibilityProps } from './compatibility.types';
+
+import { Gradient } from '@/components/elements/gradient';
+import { Heading } from '@/components/elements/heading';
+import { DownloadButton } from '@/components/modules/download-button';
+
+export const IntegrationCompatibility = ({ title, subtitle, tools }: IntegrationCompatibilityProps) => {
+  return (
+    <StyledIntegrationCompatibilityWrapper>
+      <StyledIntegrationCompatibilityGradientWrapper>
+        <Gradient $shape="full" />
+      </StyledIntegrationCompatibilityGradientWrapper>
+      <StyledIntegrationCompatibilityContentWrapper>
+        <StyledIntegrationCompatibilityContent>
+          <StyledIntegrationCompatibilityContentTitle>
+            <StyledIntegrationCompatibilityLabel>{subtitle}</StyledIntegrationCompatibilityLabel>
+            <Heading as="h2" fontSize="l" color="textGradient">
+              {title}
+            </Heading>
+          </StyledIntegrationCompatibilityContentTitle>
+          <DownloadButton $small $variant="secondary" />
+        </StyledIntegrationCompatibilityContent>
+        <StyledCompatibilityBadgeWrapper>
+          {Array.isArray(tools) &&
+            tools.length > 0 &&
+            tools.map(tool => <CompatibilityBadge>{tool}</CompatibilityBadge>)}
+        </StyledCompatibilityBadgeWrapper>
+      </StyledIntegrationCompatibilityContentWrapper>
+    </StyledIntegrationCompatibilityWrapper>
+  );
+};
