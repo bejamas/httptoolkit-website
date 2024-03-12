edebb6e02e19e63df0353014b62f01e0849e2762
Gerald Martinez <gerald@bejamas.io>
2024-03-11 22:38:32 -0600

    feat(HTD-54): add related posts

---
+++ src/components/sections/blog/related-posts/index.tsx
@@ -0,0 +1,40 @@
+import { StyledRelatedPostWrapper, StyledRelatedPostsGrid } from './related-posts.styles';
+
+import Stack from '@/components/elements/stack';
+import { BlogCard } from '@/components/modules/blog-card';
+import { HeadingBlock } from '@/components/modules/heading-block';
+import { getRelatedPosts } from '@/lib/mdx';
+
+interface RelatedPostsProps {
+  tags: string[];
+}
+
+export const RelatedPosts = async ({ tags }: RelatedPostsProps) => {
+  const relatedPosts = await getRelatedPosts(tags);
+
+  if (relatedPosts.length === 0) {
+    return null;
+  }
+
+  return (
+    <StyledRelatedPostWrapper>
+      <Stack $gap="32px" $gapxl="48px">
+        <HeadingBlock title="Related `*content*`" $align="center" $isContentCentered />
+        <StyledRelatedPostsGrid>
+          {relatedPosts.length &&
+            relatedPosts.map(post => (
+              <BlogCard
+                title={post.title}
+                slug={post.slug}
+                image={{ src: `/images/${post.coverImage}`, alt: post.title }}
+                date={post.date}
+                tag={post.tags[0]}
+                // TODO(gerald): Replace with the excerpt when working on blog post content
+                text="Sometimes things crash when they're running inside a Docker container though, and then all of a sudden it can get much more difficult to work out why, or what the hell to do next."
+              />
+            ))}
+        </StyledRelatedPostsGrid>
+      </Stack>
+    </StyledRelatedPostWrapper>
+  );
+};
--- src/lib/mdx/index.ts
+++ src/lib/mdx/index.ts
@@ -65,3 +65,16 @@ export const getAllPostsMeta = async () => {
   }
   return posts;
 };
+
+export const getRelatedPosts = async (tags: string[]) => {
+  const allPosts = await getAllPostsMeta();
+
+  const filterdPosts = allPosts
+    .filter(post => {
+      return tags.some(tag => post.tags.includes(tag));
+    })
+    .sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime())
+    .slice(0, 3);
+
+  return filterdPosts;
+};
