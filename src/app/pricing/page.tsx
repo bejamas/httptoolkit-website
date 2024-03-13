1fff31d2801c2695dcad3c5bf3a8e7cab2a5c783
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-13 10:25:51 -0400

    feat(HTD-57): add pricing section

--- src/app/pricing/page.tsx
+++ src/app/pricing/page.tsx
@@ -1,10 +1,14 @@
 import { Container } from '@/components/elements/container';
 import { Heading } from '@/components/elements/heading';
+import { PricingPlans } from '@/components/sections/pricing/plans';
 
 export default function PricingPage() {
   return (
-    <Container>
-      <Heading color="textGradient">Pricing</Heading>
-    </Container>
+    <>
+      <Container>
+        <Heading color="textGradient">Pricing</Heading>
+      </Container>
+      <PricingPlans />
+    </>
   );
 }
