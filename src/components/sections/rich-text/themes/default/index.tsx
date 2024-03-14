3d8a209a5adc66576150e10673bece6e438cc93a
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-14 09:11:33 -0400

    chore(HTD-48): add comment to add default styles

--- src/components/sections/rich-text/themes/default/index.tsx
+++ src/components/sections/rich-text/themes/default/index.tsx
@@ -1,5 +1,6 @@
 import { StyledLink } from './default.styles';
 
+// TODO: need to define the default styles
 export const defaultComponents = {
   a({ children, href }: Component<{ href: string }>) {
     return <StyledLink href={href}>{children}</StyledLink>;
