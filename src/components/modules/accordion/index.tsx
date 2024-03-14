44f8d3e4935a3844775b2734e50be9aae0d79612
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-14 10:32:54 -0400

    fix(HTD-59): remove double header

--- src/components/modules/accordion/index.tsx
+++ src/components/modules/accordion/index.tsx
@@ -1,7 +1,5 @@
 'use client';
 
-import * as RadixAccordion from '@radix-ui/react-accordion';
-
 import {
   StyledAccordionContent,
   StyledAccordionItem,
@@ -20,14 +18,12 @@ export const Accordion = ({ items, $variant = 'default' }: AccordionProps) => {
         items.length > 0 &&
         items.map(item => (
           <StyledAccordionItem value={item.title} $variant={$variant}>
-            <RadixAccordion.Header>
-              <StyledAccordionTrigger>
-                <Heading as="h3" fontSize="s" color="darkGrey" textAlign="left">
-                  {item.title}
-                </Heading>
-                <CaretDown weight="fill" size={24} />
-              </StyledAccordionTrigger>
-            </RadixAccordion.Header>
+            <StyledAccordionTrigger>
+              <Heading as="h3" fontSize="s" color="darkGrey" textAlign="left">
+                {item.title}
+              </Heading>
+              <CaretDown weight="fill" size={24} />
+            </StyledAccordionTrigger>
             <StyledAccordionContent>{item.text}</StyledAccordionContent>
           </StyledAccordionItem>
         ))}
