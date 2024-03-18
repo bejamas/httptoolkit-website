f047d878ca28c146eae99f0c3a818ebfaabbaa28
Gerald Martinez <gerald@bejamas.io>
2024-03-18 07:44:20 -0600

    feat(HTD-76): add HttpToolkitFeatures for alternative pages

--- src/app/(alternatives)/layout.tsx
+++ src/app/(alternatives)/layout.tsx
@@ -4,6 +4,7 @@ import { MockResponseFeatures } from '@/components/common-sections/mock-response
 import { RewriteAnything } from '@/components/common-sections/rewrite-anything';
 import { TryItForYourselfCTA } from '@/components/common-sections/try-it-for-yourself';
 import { Layout } from '@/components/layout';
+import { HttpToolkitFeatures } from '@/components/sections/alternatives/http-toolkit-features';
 import { Statistics } from '@/components/sections/statistics';
 import { Testimonials } from '@/components/sections/testimonials';
 
@@ -12,6 +13,7 @@ export default function AlternativesLayout({ children }: { children: React.React
     <Layout>
       {children}
       <TryItForYourselfCTA />
+      <HttpToolkitFeatures />
       <MockResponseFeatures />
       <RewriteAnything />
       <Suspense>
