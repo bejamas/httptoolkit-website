7bc91d8dc00c4306b5c0fd1f415b9803a32dee75
Gerald Martinez <gerald@bejamas.io>
2024-03-18 07:28:06 -0600

    feat(HTD-74): adds AlternativesLayout and pages

---
+++ src/app/(alternatives)/layout.tsx
@@ -0,0 +1,26 @@
+import { Suspense } from 'react';
+
+import { MockResponseFeatures } from '@/components/common-sections/mock-response-features';
+import { RewriteAnything } from '@/components/common-sections/rewrite-anything';
+import { TryItForYourselfCTA } from '@/components/common-sections/try-it-for-yourself';
+import { Layout } from '@/components/layout';
+import { Statistics } from '@/components/sections/statistics';
+import { Testimonials } from '@/components/sections/testimonials';
+
+export default function AlternativesLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <Layout>
+      {children}
+      <TryItForYourselfCTA />
+      <MockResponseFeatures />
+      <RewriteAnything />
+      <Suspense>
+        <Testimonials />
+      </Suspense>
+      <Suspense>
+        <Statistics title="Why `*HTTP Toolkit*`?" text="Numbers that speak for themselves:" />
+      </Suspense>
+      <TryItForYourselfCTA variant="cta-fluid" />
+    </Layout>
+  );
+}
