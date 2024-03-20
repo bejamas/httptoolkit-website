5e06273dc53af3d239fd7523ce87464a188d0c3a
Gerald Martinez <gerald@bejamas.io>
2024-03-20 07:49:49 -0600

    feat: inital changes

--- src/components/modules/dropdown/dropdown.styles.ts
+++ src/components/modules/dropdown/dropdown.styles.ts
@@ -1,46 +1,52 @@
 'use client';
 
+import * as HoverCard from '@radix-ui/react-hover-card';
+
 import type { DropdownOptionProps, DropdownProps } from './dropdown.types';
 
 import { Link } from '@/components/elements/link';
 import { css, screens, styled } from '@/styles';
 
-const openDropdown = css`
-  padding: 4px;
-  max-height: fit-content;
-  box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.button.border};
+export const DropdownHoverTrigger = styled.div`
+  position: relative;
 `;
 
-export const DropdownOptionsWrapper = styled.div<Pick<DropdownProps, '$direction'>>`
+export const DropdownOptionsWrapper = styled.div<Pick<DropdownProps, '$direction' | '$variant' | '$withBorder'>>`
   display: grid;
-  position: absolute;
   top: ${({ $direction }) => ($direction === 'bottom' ? 'calc(100% + 4px)' : 'auto')};
   bottom: ${({ $direction }) => ($direction === 'top' ? 'calc(100% + 4px)' : 'auto')};
   border-radius: 12px;
   background: ${({ theme }) => theme.colors.inkBlack};
-  padding: 0 4px;
+  padding: 4px;
   gap: 4px;
-  min-width: 100%;
-  max-height: 0;
-  transition: all 0.5s linear;
-  overflow: hidden;
-  z-index: 33;
-  box-shadow: '0px 0px 8px 0px rgba(230, 232, 242, 0.05)';
 
-  &:hover {
-    ${openDropdown}
-  }
+  ${({ $variant, $withBorder }) => {
+    switch ($variant) {
+      case 'primary':
+        return css`
+          width: calc(100% + ${$withBorder ? '2rem' : '1rem'});
+        `;
+      case 'secondary':
+        return css`
+        width:   width: calc(100% + 1rem);
+      `;
+      default:
+        return css`
+          width: calc(100% + 1rem);
+        `;
+    }
+  }};
+
+  max-height: fit-content;
+  z-index: 33;
+  box-shadow: 0 0 0 1px ${({ theme }) => theme.colors.button.border};
 `;
 
-export const DropdownWrapper = styled.div<Pick<DropdownProps, '$variant'>>`
+export const DropdownWrapper = styled(HoverCard.Root)<Pick<DropdownProps, '$variant'>>`
   position: relative;
   width: 100%;
   border-radius: 12px;
   justify-content: center;
-
-  &:hover ${DropdownOptionsWrapper}, &:focus-within ${DropdownOptionsWrapper} {
-    ${openDropdown}
-  }
 
   @media (min-width: ${screens['lg']}) {
     justify-content: start;
@@ -59,6 +65,7 @@ const baseOption = css<DropdownOptionProps>`
   text-align: center;
   text-decoration: none;
   outline: none;
+  transition: all 0.3s linear;
 
   @media (min-width: ${screens['lg']}) {
     text-align: left;
@@ -76,4 +83,8 @@ export const LinkDropdownOption = styled(Link)<DropdownOptionProps>`
 
 export const DropdownOption = styled.button<DropdownOptionProps>`
   ${baseOption}
+`;
+
+export const HoverCardContent = styled(HoverCard.Content)`
+  animation-duration: 400ms;
 `;
--- src/components/modules/dropdown/index.tsx
+++ src/components/modules/dropdown/index.tsx
@@ -1,8 +1,16 @@
 'use client';
 
 import { CaretDown } from '@phosphor-icons/react/dist/ssr';
+import * as HoverCard from '@radix-ui/react-hover-card';
 
-import { DropdownOption, DropdownOptionsWrapper, DropdownWrapper, LinkDropdownOption } from './dropdown.styles';
+import {
+  DropdownHoverTrigger,
+  DropdownOption,
+  DropdownOptionsWrapper,
+  DropdownWrapper,
+  HoverCardContent,
+  LinkDropdownOption,
+} from './dropdown.styles';
 import type { DropdownOptionProps, DropdownProps, OptionComponentType } from './dropdown.types';
 
 import { Button } from '@/components/elements/button';
@@ -31,12 +39,25 @@ export const Dropdown = ({
 }: Component<DropdownProps>) => {
   return (
     <DropdownWrapper $variant={$variant}>
-      <Button icon={icon} iconWeight={iconWeight} $variant={$variant} $isDropdown {...buttonProps}>
-        {children}
-      </Button>
-      <DropdownOptionsWrapper $direction={$direction} role="menu">
-        {Array.isArray(items) && renderOptions(items, $variant)}
-      </DropdownOptionsWrapper>
+      <HoverCard.Trigger asChild>
+        <DropdownHoverTrigger>
+          <Button as="button" icon={icon} iconWeight={iconWeight} $variant={$variant} $isDropdown {...buttonProps}>
+            {children}
+          </Button>
+        </DropdownHoverTrigger>
+      </HoverCard.Trigger>
+      <HoverCard.Portal>
+        <HoverCardContent align="start" sideOffset={5}>
+          <DropdownOptionsWrapper
+            $direction={$direction}
+            $variant={$variant}
+            $withBorder={buttonProps.$withBorder}
+            role="menu"
+          >
+            {Array.isArray(items) && renderOptions(items, $variant)}
+          </DropdownOptionsWrapper>
+        </HoverCardContent>
+      </HoverCard.Portal>
     </DropdownWrapper>
   );
 };
