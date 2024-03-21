ecc1ae95a5f98bf973f6759088c2e3561d276756
Ricardo Boraure <ricardo.boraure@bejamas.io>
2024-03-21 12:06:41 -0400

    fix(HTD-84): remove unneeded invocation

--- src/app/(documentation)/docs/page.tsx
+++ src/app/(documentation)/docs/page.tsx
@@ -1,14 +1,10 @@
 import { Container } from '@/components/elements/container';
 import { Heading } from '@/components/elements/heading';
-import { getAllDocsMeta } from '@/lib/mdx/docs';
 
 export default async function DocsPage() {
-  const docs = await getAllDocsMeta();
-
   return (
     <Container>
       <Heading color="textGradient">Docs</Heading>
-      {docs[2].content}
     </Container>
   );
 }
