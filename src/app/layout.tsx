6f081a2b597eec075c67a57296978c252c318fc8
Gerald Martinez <gerald@bejamas.io>
2024-03-28 04:53:54 -0600

    feat(HTD-94): use viewport object

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
-import type { Metadata } from 'next';
 import { DM_Sans } from 'next/font/google';
+import type { Metadata, Viewport } from 'next/types';
 
 import { Button } from '@/components/elements/button';
 import { RadixProviders } from '@/components/layout/radix-layout';
@@ -28,6 +28,11 @@ export const metadata: Metadata = {
   },
 };
 
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+};
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -35,7 +40,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
       <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
       <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
