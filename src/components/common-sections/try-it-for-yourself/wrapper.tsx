60bcff1423c130425fde253d8d6d95547531bd0e
Gerald Martinez <gerald@bejamas.io>
2024-04-15 20:07:04 -0600

    feat(HTD-115): just triggerOnce

--- src/components/common-sections/try-it-for-yourself/wrapper.tsx
+++ src/components/common-sections/try-it-for-yourself/wrapper.tsx
@@ -6,6 +6,7 @@ import { useInView } from 'react-intersection-observer';
 export const TryYourselfWrapper = ({ children }: Component) => {
   const { ref, inView } = useInView({
     threshold: 0.5,
+    triggerOnce: true,
   });
 
   return <div ref={ref}>{inView && children}</div>;
